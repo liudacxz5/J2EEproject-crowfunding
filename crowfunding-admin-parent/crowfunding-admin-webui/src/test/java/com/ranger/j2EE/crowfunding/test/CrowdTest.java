050767e184788939819fa08f0d6bdc92c4fe03ad
ranger_ld <liudacxz5@163>
2020-04-06 21:55:00 +0800

    配置文件编写

---
+++ crowfunding-admin-parent/crowfunding-admin-webui/src/test/java/com/ranger/j2EE/crowfunding/test/CrowdTest.java
@@ -0,0 +1,27 @@
+package com.ranger.j2EE.crowfunding.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+//在类上标记必要注解，Spring整合Junit
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+
+    }
+}
